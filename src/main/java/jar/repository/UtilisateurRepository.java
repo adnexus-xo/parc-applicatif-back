047b082463e273523d181491497a9a876746b7c4
adnexus-xo <aitadiagne333@gmail.com>
2026-06-22 17:09:23 +0000

    Add findByEmailAndMotDePasse method to repository

--- src/main/java/jar/repository/UtilisateurRepository.java
+++ src/main/java/jar/repository/UtilisateurRepository.java
@@ -2,9 +2,10 @@ package jar.repository;
 
 import jar.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;           
+import org.springframework.stereotype.Repository;
+
 
 //@Repository
 public interface UtilisateurRepository extends JpaRepository <Utilisateur, Integer> {
-    
+    Utilisateur findByEmailAndMotDePasse(String email, String motDePasse);
 }
