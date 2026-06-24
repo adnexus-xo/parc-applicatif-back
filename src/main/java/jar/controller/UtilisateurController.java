dfeed7a19bb2076ca3c8737bee938ea99d5fba3d
adnexus-xo <aitadiagne333@gmail.com>
2026-06-24 08:53:56 +0000

    fix: Controller of Utilisateurs

--- src/main/java/jar/controller/UtilisateurController.java
+++ src/main/java/jar/controller/UtilisateurController.java
@@ -5,12 +5,9 @@ import jar.entity.Utilisateur;
 import jar.service.UtilisateurService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/utilisateurs")
@@ -45,21 +42,6 @@ public class UtilisateurController {
         return ResponseEntity.ok(utilisateurService.save(utilisateur));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
-    String email = credentials.get("email");
-    String password = credentials.get("password");
-
-    // On cherche l'utilisateur
-    Optional<Utilisateur> user = Optional.ofNullable(utilisateurService.findByEmailAndMotDePasse(email, password));
-    
-    if (user.isPresent()) {
-        return ResponseEntity.ok(user.get());
-    } else {
-        // Renvoie une erreur 401 si les identifiants sont faux ou l'utilisateur introuvable
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou mot de passe incorrect");
-    }
-    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Utilisateur> update(@PathVariable Integer id, @RequestBody Utilisateur utilisateur) {
