99fe52f0445979a16e7f037552def7114d306fb9
adnexus-xo <aitadiagne333@gmail.com>
2026-06-24 09:09:01 +0000

    fix: query login avec @Query explicite

--- src/main/java/jar/repository/UtilisateurRepository.java
+++ src/main/java/jar/repository/UtilisateurRepository.java
@@ -1,14 +1,13 @@
 package jar.repository;
 
 import jar.entity.Utilisateur;
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
 
-//@Repository
-public interface UtilisateurRepository extends JpaRepository <Utilisateur, Integer> {
-    Optional<Utilisateur> findByEmailAndMotDePasse(String email, String motDePasse);
+    @Query("SELECT u FROM Utilisateur u WHERE u.email = :email AND u.motDePasse = :motDePasse")
+    Optional<Utilisateur> findByEmailAndMotDePasse(@Param("email") String email, @Param("motDePasse") String motDePasse);
 }
